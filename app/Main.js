3999197797b83f259b3ef8cb90afa303e1928c15
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-01 16:38:08 +0700

    2. Routing (Single Page Application)

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import About from "./components/About";
+import Terms from "./components/Terms";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us" exact>
+          <About />
+        </Route>
+        <Route path="/terms" exact>
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
