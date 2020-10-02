0b4cad90ec20d9b117643d645d27f99bcf50177c
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-02 13:19:15 +0700

    14. useReducer

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import ReactDOM from "react-dom";
 import About from "./components/About";
 import Terms from "./components/Terms";
@@ -13,30 +13,49 @@ import ViewSinglePost from "./components/ViewSinglePost";
 import FlashMessages from "./components/FlashMessages";
 import ExampleContext from "./ExampleContext";
 
-Axios.defaults.baseURL = 'http://localhost:8080'
+Axios.defaults.baseURL = "http://localhost:8080";
 
 function Main() {
-  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')));
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
+    flashMessages: [],
+  };
+  function ourReducer(state, action) {
+    switch (action.type) {
+      case "login":
+        return { loggedIn: true, flashMessages: state.flashMessages };
+      case "logout":
+        return  { loggedIn: false, flashMessages: state.flashMessages };;
+      case "flashMessage":
+        return {loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value)};
+    }
+  }
+
+  const [state, dispatch] = useReducer(ourReducer, initialState);
+
+  const [loggedIn, setLoggedIn] = useState(
+    Boolean(localStorage.getItem("complexappToken"))
+  );
   const [flashMessages, setFlashMessages] = useState([]);
 
   function addFlashMessage(msg) {
-    setFlashMessages(prev => prev.concat(msg))
+    setFlashMessages((prev) => prev.concat(msg));
   }
 
   return (
-    <ExampleContext.Provider value={{addFlashMessage, setLoggedIn}}>
+    <ExampleContext.Provider value={{ addFlashMessage, setLoggedIn }}>
       <BrowserRouter>
-        <FlashMessages messages={flashMessages}/>
-        <Header loggedIn={loggedIn}/>
+        <FlashMessages messages={flashMessages} />
+        <Header loggedIn={loggedIn} />
         <Switch>
           <Route path="/" exact>
-            { loggedIn ? <Home /> : <HomeGuest />}
+            {loggedIn ? <Home /> : <HomeGuest />}
           </Route>
           <Route path="/post/:id">
             <ViewSinglePost />
           </Route>
           <Route path="/create-post" exact>
-            <CreatePost/>
+            <CreatePost />
           </Route>
           <Route path="/about-us" exact>
             <About />
