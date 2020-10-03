8582b84b93813a0efacb115d8812d01f397ef036
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-03 18:17:09 +0700

    29. Delete a Post

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,13 +1,17 @@
 import Axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, withRouter } from "react-router-dom";
 import ReactTooltip from "react-tooltip";
+import DispatchContext from "../DispatchContext";
+import StateContext from "../StateContext";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 import NotFound from "./NotFound";
 import Page from "./Page";
 
 function ViewSinglePost(props) {
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [post, setPost] = useState();
@@ -20,7 +24,7 @@ function ViewSinglePost(props) {
         const response = await Axios.get(`/post/${id}`, {
           cancelToken: ourRequest.token,
         });
-        console.log(response.data);
+
         setPost(response.data);
         setIsLoading(false);
       } catch (error) {
@@ -33,8 +37,8 @@ function ViewSinglePost(props) {
     };
   }, []);
 
-  if(!isLoading && !post) {
-    return <NotFound />
+  if (!isLoading && !post) {
+    return <NotFound />;
   }
 
   if (isLoading)
@@ -49,30 +53,65 @@ function ViewSinglePost(props) {
     date.getMonth() + 1
   }/${date.getDate()}/${date.getFullYear()}`;
 
+  function isOwner() {
+    if (appState.loggedIn) {
+      return appState.user.username == post.author.username;
+    }
+    return false;
+  }
+
+  async function deleteHandler() {
+    const areYouSure = window.confirm(
+      "Do you really want to delete this post?"
+    );
+    if (areYouSure) {
+      try {
+        const response = await Axios.delete(`/post/${id}`, {
+          data: {
+            token: appState.user.token
+          }
+        })
+        if (response.data == "Success") {
+          appDispatch({
+            type: "flashMessage",
+            value: "Post was successfully deleted.",
+          });
+
+          props.history.push(`/profile/${appState.user.username}`);
+        }
+      } catch (error) {
+        console.log("There was a problem");
+      }
+    }
+  }
+
   return (
     <Page title={post.title}>
       <div className="container container--narrow py-md-5">
         <div className="d-flex justify-content-between">
           <h2>{post.title}</h2>
-          <span className="pt-2">
-            <Link
-              to={`/post/${post._id}/edit`}
-              data-tip="Edit"
-              data-for="edit"
-              className="text-primary mr-2"
-            >
-              <i className="fas fa-edit"></i>
-            </Link>
-            <ReactTooltip id="edit" className="custom-tooltip" />{" "}
-            <a
-              data-tip="Delete"
-              data-for="delete"
-              className="delete-post-button text-danger"
-            >
-              <i className="fas fa-trash"></i>
-            </a>
-            <ReactTooltip id="delete" className="custom-tooltip" />
-          </span>
+          {isOwner() && (
+            <span className="pt-2">
+              <Link
+                to={`/post/${post._id}/edit`}
+                data-tip="Edit"
+                data-for="edit"
+                className="text-primary mr-2"
+              >
+                <i className="fas fa-edit"></i>
+              </Link>
+              <ReactTooltip id="edit" className="custom-tooltip" />{" "}
+              <a
+                onClick={deleteHandler}
+                data-tip="Delete"
+                data-for="delete"
+                className="delete-post-button text-danger"
+              >
+                <i className="fas fa-trash"></i>
+              </a>
+              <ReactTooltip id="delete" className="custom-tooltip" />
+            </span>
+          )}
         </div>
 
         <p className="text-muted small mb-4">
@@ -105,4 +144,4 @@ function ViewSinglePost(props) {
   );
 }
 
-export default ViewSinglePost;
+export default withRouter(ViewSinglePost);
