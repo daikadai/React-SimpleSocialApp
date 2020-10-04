06ff158f94343029c88938bd93ed23bda17100aa
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-04 18:16:11 +0700

    37. Homepage Post Feed

--- app/components/ProfilePost.js
+++ app/components/ProfilePost.js
@@ -2,6 +2,7 @@ import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import LoadingDotsIcon from "./LoadingDotsIcon";
+import Post from "./Post";
 
 function ProfilePost(props) {
   const { username } = useParams();
@@ -32,18 +33,7 @@ function ProfilePost(props) {
   return (
     <div className="list-group">
       {posts.map((post) => {
-        const date = new Date(post.createdDate);
-        const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-        return (
-          <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
-            <img
-              className="avatar-tiny"
-              src={post.author.avatar}
-            />{" "}
-            <strong>{post.title}</strong>{" "}
-            <span className="text-muted small">on {dateFormatted}</span>
-          </Link>
-        );
+        return <Post post={post} key={post._id} noAuthor={true}/>
       })}
     </div>
   );
