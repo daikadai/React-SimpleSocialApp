20c5b463ec7251aaf4213e737ac50e958489972d
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-05 12:06:54 +0700

    39. Sending & Receiving Chats (Part 1)

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,59 +1,104 @@
 import React, { useContext, useEffect, useRef } from "react";
+import { useImmer } from "use-immer";
 import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
 
 function Chat(props) {
-  const chatField = useRef(null); 
+  const chatField = useRef(null);
   const appState = useContext(StateContext);
   const appDispatch = useContext(DispatchContext);
+  const [state, setState] = useImmer({
+    fieldValue: "",
+    chatMessages: [],
+  });
 
   useEffect(() => {
-    if(appState.isChatOpen) {
-       chatField.current.focus()
+    if (appState.isChatOpen) {
+      chatField.current.focus();
     }
-  },[appState.isChatOpen])
+  }, [appState.isChatOpen]);
+
+  function handleFieldChange(e) {
+    const value = e.target.value;
+    setState((draft) => {
+      draft.fieldValue = value;
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    //Send message to chat server
+
+    setState((draft) => {
+      // Add message to state of collection messages
+      draft.chatMessages.push({
+        message: draft.fieldValue,
+        username: appState.user.username,
+        avatar: appState.user.avatar,
+      });
+      draft.fieldValue = "";
+    });
+  }
 
   return (
     <div
       id="chat-wrapper"
-      className={"chat-wrapper shadow border-top border-left border-right " + (appState.isChatOpen ? "chat-wrapper--is-visible" : '')}
+      className={
+        "chat-wrapper shadow border-top border-left border-right " +
+        (appState.isChatOpen ? "chat-wrapper--is-visible" : "")
+      }
     >
       <div className="chat-title-bar bg-primary">
         Chat
-        <span onClick={() => appDispatch({ type: 'closeChat'})} className="chat-title-bar-close">
+        <span
+          onClick={() => appDispatch({ type: "closeChat" })}
+          className="chat-title-bar-close"
+        >
           <i className="fas fa-times-circle"></i>
         </span>
       </div>
       <div id="chat" className="chat-log">
-        <div className="chat-self">
-          <div className="chat-message">
-            <div className="chat-message-inner">Hey, how are you?</div>
-          </div>
-          <img
-            className="chat-avatar avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />
-        </div>
+        {state.chatMessages.map((message, index) => {
+          if (message.username == appState.user.username) {
+            return (
+              <div className="chat-self">
+                <div className="chat-message">
+                  <div className="chat-message-inner">{message.message}</div>
+                </div>
+                <img className="chat-avatar avatar-tiny" src={message.avatar} />
+              </div>
+            );
+          }
 
-        <div className="chat-other">
-          <a href="#">
-            <img
-              className="avatar-tiny"
-              src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-            />
-          </a>
-          <div className="chat-message">
-            <div className="chat-message-inner">
+          return (
+            <div className="chat-other">
               <a href="#">
-                <strong>barksalot:</strong>
+                <img
+                  className="avatar-tiny"
+                  src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
+                />
               </a>
-              Hey, I am good, how about you?
+              <div className="chat-message">
+                <div className="chat-message-inner">
+                  <a href="#">
+                    <strong>barksalot:</strong>
+                  </a>
+                  Hey, I am good, how about you?
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-      <form id="chatForm" className="chat-form border-top">
+      <form
+        onSubmit={handleSubmit}
+        id="chatForm"
+        className="chat-form border-top"
+      >
         <input
+          onChange={handleFieldChange}
+          value={state.fieldValue}
           ref={chatField}
           type="text"
           className="chat-field"
