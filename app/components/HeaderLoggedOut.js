928d3827fe10b376e1340ea972ae7335acafa4ea
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-05 18:13:02 +0700

    45. Quick Flash Message Details

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -15,8 +15,10 @@ function HeaderLoggedOut(props) {
       const response = await Axios.post('/login', {username, password});
       if(response.data) {
         appDispatch({ type: 'login', data: response.data})
+        appDispatch({ type: 'flashMessage', value: 'You have successfully logged in.'})
       } else {
         console.log('Incorrect username/password.');
+        appDispatch({ type: 'flashMessage', value: 'Invalid username / password.'})
       }
     } catch (error) {
       console.log('There was a problem');
